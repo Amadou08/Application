6118c1e6cf731bce5493ad2488ebe6f1ccf987e4
Amadou08 <145312045+Amadou08@users.noreply.github.com>
2023-10-30 23:53:45 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NOVIGRAD2505"
+include(":app")
