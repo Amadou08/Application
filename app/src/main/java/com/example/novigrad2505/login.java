54652ebec7bd1c5df694a9f703b025ec21786ad1
Amadou08 <145312045+Amadou08@users.noreply.github.com>
2023-10-31 19:11:32 -0400

    AppliNOVIGRAD DONE

--- app/src/main/java/com/example/novigrad2505/login.java
+++ app/src/main/java/com/example/novigrad2505/login.java
@@ -3,12 +3,16 @@ package com.example.novigrad2505;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class login extends AppCompatActivity {
+    private TextView Welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Welcome = findViewById(R.id.welcome);
     }
 }
