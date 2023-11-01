1959082cd09f3fd5fc8a3ab019eb5da79329ef05
asava079 <146129480+asava079@users.noreply.github.com>
2023-10-31 20:12:40 -0400

    Update login.java
    Add welcome text

--- app/src/main/java/com/example/novigrad2505/login.java
+++ app/src/main/java/com/example/novigrad2505/login.java
@@ -3,16 +3,22 @@ package com.example.novigrad2505;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class login extends AppCompatActivity {
-    private TextView Bienvenu;
+    private TextView Welcome;
+    private EditText edUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Bienvenu = findViewById(R.id.welcome);
+        Welcome = findViewById(R.id.welcome);
+        edUsername = findViewById(R.id.ed_username);
+        setContextView(R.layout.activity_login);
+        Welcome = findViewById(R.id.welcome);
+        Welcome.setText("Hi "+ edUsername);
     }
 }
